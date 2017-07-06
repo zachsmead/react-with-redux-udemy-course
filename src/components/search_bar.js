fdcb584e36449120c5a7a089bac8d6f8e95bfc33
= <zrsmead@gmail.com>
2017-07-05 19:21:12 -0700

    adding render components

---
+++ src/components/search_bar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+	render() {
+		return <input />;
+	}
+}
+
+export default SearchBar; // this is what we are sending to index.js in the import statement
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from 'react'; // we didn't write this so don't have to make a path reference- it's a library
 import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'; // we wrote this so we have to make a path reference to import
+
+const API_KEY = 'AIzaSyC21wbRLnZWiouqP10wgS8x6etgKzYJuXc';
+
 
 // Create a new component, this component should produce some html.
 
-const App = function() {
-	return <div>Hi! </div>;
+
+const App = () => {
+	return (
+		<div>
+			Hi!
+			<SearchBar />
+		</div>
+	);
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
-// App; // 'App' is a class, '<App />' is an instance of that class.
-ReactDOM.render(<App />, document.querySelector('.container')); // find the class 'container'
-																															 //	and render the App
-																															 // instance inside of it.
+// App 'App' is a class, '<App />' is an instance of that class
+ReactDOM.render(<App />, document.querySelector('.container'));	// find the class 'container'
+																															 	//	and render the App
+																															 	// instance inside of it.
