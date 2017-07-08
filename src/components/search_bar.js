ac6faf496b765b687d26b7c31f5bc5e72f2e076a
= <zrsmead@gmail.com>
2017-07-07 23:33:07 -0700

    dynamic updating with text input

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 
 class SearchBar extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
 	// render() {
 	// 	return <input onChange={this.onInputChange} />;
 	// }
@@ -14,7 +20,12 @@ class SearchBar extends Component {
 
 
 	render() {
-		return <input onChange = {event => console.log(event.target.value) } />;
+		return (
+			<div>
+				<input onChange = {event => this.setState({ term: event.target.value }) } />
+				Value of the input: { this.state.term }
+			</div>
+		);
 	}
 }
 
